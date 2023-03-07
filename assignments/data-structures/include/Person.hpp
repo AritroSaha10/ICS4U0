174332a496dfa876e7689282404dad242476984e
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-03-07 15:52:11 -0500

    feat: complete Vehicle source

--- assignments/data-structures/include/Person.hpp
+++ assignments/data-structures/include/Person.hpp
@@ -2,28 +2,32 @@
 #include <string>
 #include <vector>
 #include "BankAccount.hpp"
+#include "Vehicle.hpp"
 
 class Person {
 public:
-    Person();
+    Person(std::string firstName, std::string middleName, std::string lastName, int birthTimestamp, double height);
+    Person(std::string firstName, std::string middleName, std::string lastName, int birthTimestamp, double height, BankAccount* bankAccount);
     std::string getFirstName();
     bool setFirstName(std::string newName);
     std::string getMiddleName();
     bool setMiddleName(std::string newName);
     std::string getLastName();
     bool setLastName(std::string newName);
-    int getBirthTimestamp();
+    int getBirthTimestamp() const;
     bool changeHeight(double delta);
-    double getAge();
+    double getAge() const;
     bool addBankAccount(BankAccount* account);
     bool removeBankAccount();
     std::string serializeToJSON();
+
+    friend class Vehicle;
 
 private:
     std::string firstName;
     std::string middleName;
     std::string lastName;
-    int birthTimestamp;
+    int64_t birthTimestamp;
     double height;
     BankAccount* bankAccount;
 };
