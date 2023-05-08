5262051427758f3d5cad64f39f6425213ac646f5
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2023-05-08 13:10:46 -0400

    fix(recursion): formatting

--- assignments/recursion/main.py
+++ assignments/recursion/main.py
@@ -1,8 +1,8 @@
 # -----------------------------------------------------------------------------
 # Name:        Tower of Hanoi Solver
-# Purpose:     Solves the Tower of Hanoi puzzle using a recursive 
-#               approach. It also allows you to customize the problem 
-#               itself by specifying the number of disks, where they are, 
+# Purpose:     Solves the Tower of Hanoi puzzle using a recursive
+#               approach. It also allows you to customize the problem
+#               itself by specifying the number of disks, where they are,
 #               and where they should be moved.
 #
 # Author:      Aritro Saha
@@ -16,19 +16,16 @@ class MoveNotPossibleException(Exception):
     Raised when a Tower of Hanoi disc move is not possible.
     """
 
-    pass
-
 
 # Rods go from bottom to top
 rods: dict[str, list[int]] = {"A": [], "B": [], "C": []}
-
-# Counts how many moves were made in total
-total_moves = 0
+moves: list[tuple[int, str, str, str]] = []
 
 
-def move_disc(from_rod: str, to_rod: str, see_moves: bool = True) -> None:
+def move_disc(from_rod: str, to_rod: str) -> None:
     """
-    Moves the top disc of one rod to another, while making sure that the move is possible and follows the rules of Tower of Hanoi.
+    Moves the top disc of one rod to another, while making sure that the move is possible and
+    follows the rules of Tower of Hanoi.
 
     Parameters
     ------------
@@ -42,11 +39,10 @@ def move_disc(from_rod: str, to_rod: str, see_moves: bool = True) -> None:
     MoveNotPossibleException
         If the disc move desired is not possible.
     """
-    global total_moves
 
     if from_rod not in rods or to_rod not in rods:
         # Can't move a disc on a non-existent rod
-        raise MoveNotPossibleException(f"from_rod or to_rod does not exist")
+        raise MoveNotPossibleException("from_rod or to_rod does not exist")
 
     if len(rods[from_rod]) == 0:
         # Can't move a non-existent disc
@@ -55,7 +51,8 @@ def move_disc(from_rod: str, to_rod: str, see_moves: bool = True) -> None:
     # Get and remove the topmost disc from its rod
     disc_to_move = rods[from_rod].pop()
     if len(rods[to_rod]) > 0 and disc_to_move > rods[to_rod][-1]:
-        # There are disc(s) on the to_rod that are smaller than the disc we'd like to move, which would be an illegal move
+        # There are disc(s) on the to_rod that are smaller than the disc
+        # we'd like to move, which would be an illegal move
         raise MoveNotPossibleException(
             f"Top disc on rod {from_rod} cannot be moved on top of rod {to_rod}"
         )
@@ -63,29 +60,22 @@ def move_disc(from_rod: str, to_rod: str, see_moves: bool = True) -> None:
     # Move the disc onto the desired rod
     rods[to_rod].append(disc_to_move)
 
-    # Log the move in the console
-    if see_moves:
-        print(
-            f"Step {total_moves+1}: Move disc {disc_to_move} from rod {from_rod} to {to_rod}"
-        )
-        print_towers()
-        print()
-
-    # Increment move counter
-    total_moves += 1
+    # Remember the move later in case if the user wants to see them
+    moves.append((disc_to_move, from_rod, to_rod, get_tower_view()))
 
 
-def print_towers() -> None:
+def get_tower_view() -> str:
     """
     Print the current state of the Tower of Hanoi rods.
     """
-    for rod in rods:
-        print(f"{rod}: {' '.join([str(x) for x in rods[rod]])}")
+
+    view = ""
+    for name, val in rods.items():
+        view += f"{name}: {' '.join([str(x) for x in val])}"
+    return view
 
 
-def solve_tower_of_hanoi(
-    disc_to_move: int, from_rod: str, to_rod: str, see_moves: bool
-) -> None:
+def solve_tower_of_hanoi(disc_to_move: int, from_rod: str, to_rod: str) -> None:
     """
     Recursively solve a Tower of Hanoi puzzle.
 
@@ -105,7 +95,8 @@ def solve_tower_of_hanoi(
         # There is no disc to move, so this becomes the end of the recursion stack.
         return
 
-    # Since the Tower of Hanoi puzzle only ever has three rods, the auxilary rod (spare rod used to move the discs to the desired rod) can only be the rod that isn't the from or to rod
+    # Since the Tower of Hanoi puzzle only ever has three rods, the auxilary rod (spare rod used to
+    # move the discs to the desired rod) can only be the rod that isn't the from or to rod
     aux_rod = set(rods.keys()).difference(set([from_rod, to_rod])).pop()
 
     # Move the discs on top of the disc_to_move onto the auxilary rod
@@ -113,18 +104,16 @@ def solve_tower_of_hanoi(
         disc_to_move=disc_to_move - 1,
         from_rod=from_rod,
         to_rod=aux_rod,
-        see_moves=see_moves,
     )
 
     # Move the disc_to_move onto the desired rod (to_rod)
-    move_disc(from_rod=from_rod, to_rod=to_rod, see_moves=see_moves)
+    move_disc(from_rod=from_rod, to_rod=to_rod)
 
     # Move the discs we moved onto the auxilary rod back on top of the disc_to_move
     solve_tower_of_hanoi(
         disc_to_move=disc_to_move - 1,
         from_rod=aux_rod,
         to_rod=to_rod,
-        see_moves=see_moves,
     )
 
 
@@ -156,28 +145,28 @@ if __name__ == "__main__":
     print()
 
     # Generate a list of the discs from greatest to least
-    rods[starting_rod] = [i for i in range(disc_count, 0, -1)]
+    rods[starting_rod] = list(range(disc_count, 0, -1))
 
     # Display the starting state of the rods
     print("Starting state of rods:")
-    print_towers()
+    print(get_tower_view())
     print()
 
     # Find the disc that should be moved
-    disc_to_move = -1
-    while disc_to_move == -1 and (
+    disc_to_move_inp = -1
+    while disc_to_move_inp == -1 and (
         (tmp := input("What disc should be moved? ")) or tmp == ""
     ):
         if tmp.isdigit() and (tmp := int(tmp)) > 0 and tmp <= disc_count:
-            disc_to_move = tmp
+            disc_to_move_inp = tmp
         else:
             print("Please provide a valid disc to move.\n")
 
     print()
 
     # Find where the disc and those above it should move to
-    to_rod = ""
-    while to_rod == "" and (
+    to_rod_inp = ""
+    while to_rod_inp == "" and (
         (
             tmp := input(
                 "What rod should the disc and those above it end up on? (A, B, C) "
@@ -186,29 +175,38 @@ if __name__ == "__main__":
         or tmp == ""
     ):
         if tmp in ["A", "B", "C"]:
-            to_rod = tmp
+            to_rod_inp = tmp
         else:
             print("Please provide a valid rod.\n")
 
     print()
 
     # Check whether user would like to see the moves
-    see_moves = None
-    while see_moves == None and (
+    see_moves_inp = None
+    while see_moves_inp is None and (
         (tmp := input("Would you like to see the moves? (y/n) ")) or tmp == ""
     ):
         if tmp == "y":
-            see_moves = True
+            see_moves_inp = True
         elif tmp == "n":
-            see_moves = False
+            see_moves_inp = False
         else:
             print("Please provide a valid answer.\n")
 
     print()
 
     # Solve the Tower of Hanoi puzzle given the user setup
-    solve_tower_of_hanoi(disc_to_move, starting_rod, to_rod, see_moves)
+    solve_tower_of_hanoi(
+        disc_to_move_inp,
+        starting_rod,
+        to_rod_inp,
+    )
 
     print(
-        f"It takes {total_moves} to solve the Tower of Hanoi puzzle with the given constraints."
+        f"It takes {len(moves)} to solve the Tower of Hanoi puzzle with the given constraints."
     )
+
+    for i, (disc_moved, rod_from, rod_to, tower_view) in enumerate(moves):
+        print(f"Step {i+1}: Move disc {disc_moved} from rod {rod_from} to {rod_to}")
+        print(tower_view)
+        print()
